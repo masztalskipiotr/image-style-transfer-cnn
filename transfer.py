2cbf7f5ad40cc4e6f304b0ab1d8974e53e6d1d29
Piotr Masztalski <masztalski.piotr@gmail.com>
2019-04-03 17:45:21 +0200

    pep8 facelift

--- transfer.py
+++ transfer.py
@@ -30,7 +30,7 @@ def load_image(path, max_size=400, shape=None):
                                               (0.229, 0.224, 0.225))])
 
     # discard the transparent, alpha channel (that's the :3) and add the batch dimension
-    image = in_transform(image)[:3,:,:].unsqueeze(0)
+    image = in_transform(image)[:3, :, :].unsqueeze(0)
 
     return image
 
@@ -52,7 +52,7 @@ def get_features(image, model, style_layers=None, content_layers=None):
 
     if style_layers is None and content_layers is None:
         style_layers = config_data['style_layers']
-        content_layers = config_data['content_layers'] 
+        content_layers = config_data['content_layers']
                     
     features = {}
     x = image
@@ -61,7 +61,7 @@ def get_features(image, model, style_layers=None, content_layers=None):
         x = layer(x)
         if layer_idx in style_layers:
             # get the layer_name (key), without the weight (value)
-            layer_name= ''.join(*style_layers[layer_idx])
+            layer_name = ''.join(*style_layers[layer_idx])
             features[layer_name] = x
         elif layer_idx in content_layers:
             features[content_layers[layer_idx]] = x
@@ -90,14 +90,16 @@ vgg.to(device)
 # load in content and style image
 content = load_image(config_data['paths']['content_image_path']).to(device)
 # resize style image to match content image
-style = load_image(config_data['paths']['style_image_path'], shape=content.shape[-2:]).to(device)
+style = load_image(config_data['paths']['style_image_path'],
+                   shape=content.shape[-2:]).to(device)
 
 # get desired content and style features
 content_features = get_features(content, vgg)
 style_features = get_features(style, vgg)
 
 # calculate gram matrices for style representation layers
-style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+style_grams = {layer: gram_matrix(style_features[layer])
+               for layer in style_features}
 
 # prepare a copy of the content image that will be iteratively altered
 target = content.clone().requires_grad_(True).to(device)
@@ -112,9 +114,9 @@ steps = config_data['num_steps']
 
 for ii in range(1, steps+1):
     target_features = get_features(target, vgg)
-    content_loss = torch.mean((content_features['conv4_2'] - \
+    content_loss = torch.mean((content_features['conv4_2'] -
                                target_features['conv4_2']) ** 2)
-    
+
     style_loss = 0
 
     for layer_idx, layer in config_data['style_layers'].items():
@@ -125,7 +127,8 @@ for ii in range(1, steps+1):
         target_gram = gram_matrix(target_feature)
         style_gram = gram_matrix(style_feature)
 
-        layer_style_loss = torch.mean((style_gram - target_gram) ** 2) * list(layer.values())[0]
+        layer_style_loss = torch.mean((style_gram - target_gram) ** 2) * \
+                           list(layer.values())[0]
         print(list(layer.values())[0])
         style_loss += layer_style_loss / (d * h * w)
 
